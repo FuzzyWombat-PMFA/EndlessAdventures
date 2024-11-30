db7ea847e8de32dc1db35136a8ceae5c79056191
FuzzyWombat-PMFA <juli1.mand1@gmail.com>
2024-11-30 15:54:34 +0100

    ModifJeudiCodeDeplacement

--- RogueLikeVR/Assets/Code/DeplacementSalleTest.cs
+++ RogueLikeVR/Assets/Code/DeplacementSalleTest.cs
@@ -4,21 +4,40 @@ using UnityEngine;
 
 public class DeplacementSalleTest : MonoBehaviour
 {
+    //Déclaration des variables salles (à mettre dans l'inspecteur)
+
     public GameObject Salle1;
     public GameObject Salle2;
     public GameObject Salle3;
     public GameObject Salle4;
-    
+
+    //Parité de la salle pour éviter des problèmes avec le backtracking
+
+    public static bool SalleActuPaire = true;
+    //public static bool Paire = true;
+
+    //Suivi de position de la salle et de la salle précedente
+
     static int PositionX = 0;
     static int PositionZ = 0;
 
+    static int PositionPrécedenteX = 0;
+    static int PositionPrécedenteZ = 0;
 
-    static List<GameObject> ListeSalles = new List<GameObject>();
+    // Variables publiques pour le positionement de la nouvelle salle
+
+    public static int AjoutX = 0;
+    public static int AjoutZ = 0;
+
+    // Définition de la liste des salles disponibles, le code en choisis une au hasard
+
+    static List<GameObject> ListeSalles = new List<GameObject>() {};
 
     //static List<int> MapSalles = new List<int>();
+
     static List<object> MapSalles = new List<object>();
 
-
+    // Définition de la fonction de déplacement d'objets
 
 
     public static void MoveGameObject(GameObject A,int X,int Z)
@@ -26,10 +45,55 @@ public class DeplacementSalleTest : MonoBehaviour
         A.transform.position = new Vector3(X*-33.7484f, -0.5f, Z* -33.7484f);
     }
 
-    public static void GenerationSalleOuest()
-    {
+    // Fonctions directions
 
-        PositionZ = PositionZ + 1;
+    public static void DirectionOuest()
+    {
+        AjoutZ = 1;
+        AjoutX = 0;
+    }
+    public static void DirectionEst()
+    {
+        AjoutX = 0;
+        AjoutZ = -1;
+    }
+    public static void DirectionSud()
+    {
+        AjoutX = -1;
+        AjoutZ = 0;
+    }
+    public static void DirectionNord()
+    {
+        AjoutX = 1;
+        AjoutZ = 0;
+    }
+
+    // Définition de la fonction de géneration et placement des salles
+
+    public static void GenerationSalle(bool Paire)
+    {
+        
+        if (SalleActuPaire == Paire)
+        {
+            Debug.Log("Vrai");
+
+            PositionZ += AjoutZ;
+            PositionX += AjoutX;
+        }
+        else
+        {
+            Debug.Log("Faux");
+
+
+            PositionX = PositionPrécedenteX;
+            PositionZ = PositionPrécedenteZ;
+
+            PositionX += AjoutX;
+            PositionZ += AjoutZ;
+        }
+
+        PositionPrécedenteX = PositionX - AjoutX;
+        PositionPrécedenteZ = PositionZ - AjoutZ;
 
 
         int nombreAleatoire = Random.Range(0, 4);
@@ -37,13 +101,18 @@ public class DeplacementSalleTest : MonoBehaviour
 
         if ((PositionZ + PositionX) % 2 == 0)
         {
+            SalleActuPaire = true;
+
             do
             {
-                nombreAleatoire = Random.Range(0, 4);
+
+              nombreAleatoire = Random.Range(0, 4);
             } while (nombreAleatoire % 2 != 0);
         } 
         else
         {
+            SalleActuPaire = false;
+
             do
             {
                 nombreAleatoire = Random.Range(0, 4);
@@ -52,18 +121,26 @@ public class DeplacementSalleTest : MonoBehaviour
 
         GameObject NouvelleSalle = ListeSalles[nombreAleatoire];
 
+        List<int> PositionGenere = new List<int>() { PositionX, PositionZ };
 
-        List<int> PositionGenere = new List<int>() {PositionX,PositionZ};
 
         int Index = MapSalles.IndexOf(PositionGenere);
+
+        Debug.Log(Index);
+
 
         GameObject salle;
 
         if (Index==-1)
         {
-            MapSalles.Add(new List<int>() { PositionX, PositionZ });
+            
+            MapSalles.Add(PositionGenere);
+
+            
+            Index = MapSalles.IndexOf(PositionGenere);
             MapSalles.Add(new List<object>() { NouvelleSalle, new List<object>() { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } });
-            salle = (GameObject)((List<object>)MapSalles[1])[0];
+           
+            salle = (GameObject)((List<object>)MapSalles[Index+1])[0];
 
 
         } else
@@ -72,7 +149,10 @@ public class DeplacementSalleTest : MonoBehaviour
             salle = (GameObject)((List<object>)MapSalles[Index+1])[0];
 
         }
+
         MoveGameObject(salle,PositionX,PositionZ);
+
+        
     }
 
     // Start is called before the first frame update
@@ -84,9 +164,17 @@ public class DeplacementSalleTest : MonoBehaviour
         ListeSalles.Add(Salle3);
         ListeSalles.Add(Salle4);
 
-        GenerationSalleOuest();
+        DirectionOuest();
+        GenerationSalle(true);
+        DirectionNord();
+        GenerationSalle(true);
+        DirectionOuest();
+        GenerationSalle(true);
+        DirectionNord();
+        GenerationSalle(true);
+
     }
-    
+
     // Update is called once per frame
     void Update()
     {
