7c496b6a46c39cef95060435ca263209d6c238db
FuzzyWombat-PMFA <juli1.mand1@gmail.com>
2024-12-19 15:59:54 +0100

    Reapply "UpdateButton++"
    This reverts commit 83479ebd56d116e11a890e9467455d39a4d79100.

---
+++ RogueLikeVR/Assets/Code/TriggerInputDetector.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR;
+using TMPro;
+
+public class TriggerInputDetector : MonoBehaviour
+{
+
+    private InputData _inputData;
+    private float triggerValue;
+
+    private void Start()
+    {
+        _inputData = GetComponent<InputData>();
+        Debug.Log("Started inputData: " + _inputData);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (_inputData._rightController.TryGetFeatureValue(CommonUsages.primaryButton, out bool Abutton))
+        {
+            Debug.Log("A button: " + Abutton);
+        }
+
+        if (_inputData._rightController.TryGetFeatureValue(CommonUsages.secondaryButton, out bool Bbutton))
+        {
+            Debug.Log("B button: " + Bbutton);
+        }
+
+    }
+}
